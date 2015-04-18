3e2f2aa349ec963b91ec3256e4bcf773a7f6bb55
Saul Beniquez <beniquezsd@outlook.com>
2015-04-18 13:53:19 -0400

    Adding support for VIM code folding

--- libdNES/source/cpu.d
+++ libdNES/source/cpu.d
@@ -8,21 +8,20 @@ import std.bitmanip;
 
 class MOS6502
 {
-    struct StatusRegister
-    {
-        union {
-            ubyte value;
-            mixin(bitfields!(
-                ubyte, "c", 1,   // carry flag
-                ubyte, "z", 1,   // zero  flag
-                ubyte, "i", 1,   // interrupt disable flag
-                ubyte, "d", 1,   // decimal mode status (unused in NES)
-                ubyte, "b", 1,   // software interrupt flag (BRK)
-                ubyte, "",  1,   // not used. Must be logical 1 at all times.
-                ubyte, "v", 1,   // overflow flag
-                ubyte, "s", 1)); // sign     flag
-        }
+    //@region union StatusRegister
+    union StatusRegister { 
+        ubyte value;
+        mixin(bitfields!(
+            ubyte, "c", 1,   // carry flag
+            ubyte, "z", 1,   // zero  flag
+            ubyte, "i", 1,   // interrupt disable flag
+            ubyte, "d", 1,   // decimal mode status (unused in NES)
+            ubyte, "b", 1,   // software interrupt flag (BRK)
+            ubyte, "",  1,   // not used. Must be logical 1 at all times.
+            ubyte, "v", 1,   // overflow flag
+            ubyte, "s", 1)); // sign     flag
     }
+    //@endregion
 
     ubyte a;  // accumulator
     ubyte x;  // x index
@@ -39,14 +38,9 @@ unittest
     assert(cpu.status.value == 0x01);
 
     cpu.status.value = 0xFF;
-    assert(cpu.status.c == 1 &&
-           cpu.status.z == 1 &&
-           cpu.status.i == 1 &&
-           cpu.status.d == 1 &&
-           cpu.status.b == 1 && 
-           cpu.status.v == 1 &&
-           cpu.status.s == 1);
+    assert(cpu.status.c == 1 && cpu.status.z == 1 && cpu.status.i == 1 && 
+           cpu.status.d == 1 && cpu.status.b == 1 && cpu.status.v == 1 && 
+                                                     cpu.status.s == 1);
 }
 
-// ex: set expandtab ts=4 sts=4 expandtab sw=4 filetype=d : 
-
+// ex: set foldmethod=marker foldmarker=@region,@endregion expandtab ts=4 sts=4 expandtab sw=4 filetype=d : 
--- libdNES/source/memory.d
+++ libdNES/source/memory.d
@@ -11,6 +11,7 @@ class Memory
 {
     ubyte[0x10000] data; // 16KiB addressing range
 
+    // @region unittest: Memory initialization
     unittest
     {
         import std.algorithm;
@@ -21,14 +22,16 @@ class Memory
 
         assert(value == 0); 
     }
+    //@endregion
 
     ubyte[] read(ubyte address, ubyte length=1)
     {
-        uint start = address;
-        uint end   = address + length;
+        auto start = address;
+        auto end   = address + length;
 
         return data[start..end];
     }
+    // @region unittest read(ubyte, ubyte)
     unittest 
     {
         auto mem = new Memory;
@@ -40,12 +43,13 @@ class Memory
         result = mem.read(0x0, 0x4);
         assert(result[0..4] == mem.data[0..4]);
     }
+    //@endregion
     
     void write(ushort address, ubyte value)
     {
         data[address] = value;
     }
-
+    //@region unittest write(ushort, ubyte)
     unittest
     {
         auto mem = new Memory;
@@ -54,6 +58,7 @@ class Memory
 
         assert(mem.data[0xB00B..0xB00D] == [ 0xB0, 0x0B]);
     }
+    //@endregion
 
     void  write(ushort address, ubyte[] values)
     {
@@ -62,7 +67,7 @@ class Memory
 
         data[start..end] = values;
     }
-
+    // @region unittest write(ushort,ubyte[])
     unittest
     {
         auto mem = new Memory;
@@ -70,8 +75,8 @@ class Memory
     
         assert(mem.data[0xB00B..0xB00D] == [ 0xB0, 0x0B]);
     }
+    //@endregion
 }
 
 
-// ex: set expandtab ts=4 sts=4 expandtab sw=4 filetype=d : 
-
+// ex: set foldmethod=marker foldmarker=@region,@endregion expandtab ts=4 sts=4 expandtab sw=4 filetype=d : 
