0f13765810874f7abd7a626423332bcc33b0fa9a
bobomb <mofidul2@gmail.com>
2015-04-18 21:01:38 -0700

    Make status register private, add getter/setter/clear
    Make the status register private and use @property to control get/set of
    the status register. Bit 5 seems to be required to 1, perhaps we can
    control illegally or accidently setting it to a value if we decide to
    set the whole status register in 1 operation.
    Added functions to set and clear the carry flag, as a template for the
    rest of the flags?

--- libdNES/source/cpu.d
+++ libdNES/source/cpu.d
@@ -15,7 +15,7 @@ class MOS6502
             ubyte, "c", 1,   // carry flag
             ubyte, "z", 1,   // zero  flag
             ubyte, "i", 1,   // interrupt disable flag
-            ubyte, "d", 1,   // decimal mode status (unused in NES)
+            ubyte, "d", 1,   // decimal mode _status (unused in NES)
             ubyte, "b", 1,   // software interrupt flag (BRK)
             ubyte, "",  1,   // not used. Must be logical 1 at all times.
             ubyte, "v", 1,   // overflow flag
@@ -28,19 +28,64 @@ class MOS6502
     ubyte y;  // y index
     ubyte pc; // program counter
     ubyte sp; // stack pointer
-    StatusRegister status;
+    StatusRegister _status;
+	@property
+	{
+		StatusRegister status()
+		{
+			return _status;
+		}
+
+		StatusRegister status(StatusRegister newStatus)
+		{
+			if(newStatus.value & (1<<5))
+			{
+				//do we need handle this case where we are trying to set bit 5 to zero where it indicates it should be 1?
+			}
+			return _status = newStatus;
+		}
+	}
+
+	void setCarry()
+	{
+		_status.c = 0x1;
+	}
+
+	void clearCarry()
+	{
+		_status.c = 0x0;
+	}
+
+	ubyte getCarry()
+	{
+		return _status.c;
+	}
+	unittest
+	{
+		auto cpu = new MOS6502;	
+		cpu.setCarry();
+		assert(cpu._status.c == 0x1);
+		assert(cpu.getCarry() == 0x1);
+		assert(cpu._status.z == 0x0 && cpu._status.i == 0x0 && cpu._status.d == 0x0 && 
+			   cpu._status.b == 0x0 && cpu._status.v == 0x0 && cpu._status.s == 0x0 );
+		cpu.clearCarry();
+		assert(cpu._status.c == 0x0);
+		assert(cpu.getCarry() == 0x0);
+		assert(cpu._status.z == 0x0 && cpu._status.i == 0x0 && cpu._status.d == 0x0 && 
+			   cpu._status.b == 0x0 && cpu._status.v == 0x0 && cpu._status.s == 0x0 );
+	}
 }
 
 unittest
 {
     auto cpu = new MOS6502;
-    cpu.status.c = true;
-    assert(cpu.status.value == 0x01);
+    cpu._status.c = true;
+    assert(cpu._status.value == 0x01);
 
-    cpu.status.value = 0xFF;
-    assert(cpu.status.c == 1 && cpu.status.z == 1 && cpu.status.i == 1 && 
-           cpu.status.d == 1 && cpu.status.b == 1 && cpu.status.v == 1 && 
-                                                     cpu.status.s == 1);
+    cpu._status.value = 0xFF;
+    assert(cpu._status.c == 1 && cpu._status.z == 1 && cpu._status.i == 1 && 
+           cpu._status.d == 1 && cpu._status.b == 1 && cpu._status.v == 1 && 
+                                                     cpu._status.s == 1);
 }
 
 // ex: set foldmethod=marker foldmarker=@region,@endregion expandtab ts=4 sts=4 expandtab sw=4 filetype=d : 
